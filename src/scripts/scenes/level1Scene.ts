82c70eaf15348b1b8c92f2ff22373574830b7182
unknown <dwarszaw@udel.edu>
2020-04-22 16:02:24 -0400

    Updated level 1 to include rotating pipe and submit button and 180 and 270 degree options

--- src/scripts/scenes/level1Scene.ts
+++ src/scripts/scenes/level1Scene.ts
@@ -13,8 +13,14 @@ export default class Level1Scene extends Phaser.Scene {
   currentQuestionImage: Phaser.GameObjects.Image;
   button0Deg : Phaser.GameObjects.Image;
   button90Deg : Phaser.GameObjects.Image;
+  button180Deg : Phaser.GameObjects.Image;
+  button270Deg : Phaser.GameObjects.Image;
+  submitButton : Phaser.GameObjects.Image;
   counter: integer;
   answerIndex : integer;
+  roatePipe;
+  roateDegree : integer;
+  rotateInterval : integer;
   constructor() {
     super({ key: 'Level1Scene' });
 }
@@ -35,18 +41,58 @@ export default class Level1Scene extends Phaser.Scene {
     }
 
   create() {     
-    this.add.text(this.scale.width / 100 , 5, "Click on 0 or 90 degrees!", {fill : "blue" });
+    this.roateDegree = 0;
+    this.rotateInterval = 90;
+    this.add.text(this.scale.width / 100 , 5, "Rotate Pipe and Submit!", {fill : "blue" });
     console.log("Index:" + this.currentQuestionIndex);
     //console.log(this.numCorrect);
        // console.log(this.currentQuestion.answer == 0 );
-        this.currentQuestionImage = this.add.image(this.scale.width / 2 +50, this.scale.height / 2 ,
-           "question" + this.currentQuestionIndex);
-        this.currentQuestionImage.setScale(.5); 
-
-        this.button0Deg = this.add.image(this.scale.width / 2 - 50, this.scale.height / 2 + 75, "zeroDegreeButton");
-        this.button0Deg.setScale(.25);
+        this.currentQuestionImage = this.add.image(0,0 , "question" + this.currentQuestionIndex);
+        this.currentQuestionImage.setOrigin(0, 0);
+        
+        this.roatePipe = this.add.image(this.scale.width / 2 - 18, this.scale.height / 2 + 110, "pipeWithRing");
+        this.roatePipe.setScale(.1); 
+        
+        this.button0Deg = this.add.image(15, this.scale.height / 2 + 110, "zeroDegreeButton");
+        this.button0Deg.setScale(.5);
         this.button0Deg.setInteractive();
         this.answerIndex = this.questionList.questions[this.currentQuestionIndex].answer;
+
+        this.button90Deg = this.add.image(60 , this.scale.height / 2 + 110 , "ninetyDegreeButton");
+        this.button90Deg.setScale(.48);
+        this.button90Deg.setInteractive();
+
+        this.button180Deg = this.add.image(this.scale.width - 90, this.scale.height /2 + 110, 'oneEightyDegreeButton');
+        this.button180Deg.setScale(.48);
+        this.button180Deg.setInteractive();
+
+        this.button270Deg = this.add.image(this.scale.width - 30, this.scale.height /2 + 110, 'twoSeventyDegreeButton');
+        this.button270Deg.setScale(.48);
+        this.button270Deg.setInteractive();
+
+        this.button0Deg.on('pointerdown', () => {this.roatePipe.angle = 0,
+                                                 this.roateDegree = 0; console.log(this.roateDegree) } );
+        this.button90Deg.on('pointerdown', () => {this.roatePipe.angle = 90,
+                                                 this.roateDegree = 90; console.log(this.roateDegree) } );
+        this.button180Deg.on('pointerdown', () => {this.roatePipe.angle = 180
+                                                 this.roateDegree = 180; console.log(this.roateDegree) } );
+        this.button270Deg.on('pointerdown', () => {this.roatePipe.angle = 270,
+                                                 this.roateDegree = 270; console.log(this.roateDegree) } );
+        this.submitButton = this.add.image(this.scale.width/2, this.scale.height /2 + 50, 'submitButton');
+        this.submitButton.setScale(.2);
+        this.submitButton.setInteractive();
+
+        this.submitButton.on('pointerdown', () => this.buttonPress() );
+
+
+       
+
+        //do for 270 and 360
+        //submit button conditional is rotation angle degree copy from conditioals above for calling the next scene
+        //do for 270 and 360
+        //submit button conditional is rotation angle degree copy from conditioals above for calling the next scene
+
+/*       
         if (this.answerIndex == 0){
           this.button0Deg.on('pointerdown', () => this.scene.start('CorrectScreen', 
           {currentQuestionIndex: ++this.currentQuestionIndex, numCorrect: ++this.numCorrect, totalQuestions: this.totalQuestions}) );
@@ -67,9 +113,22 @@ export default class Level1Scene extends Phaser.Scene {
           this.button90Deg.on('pointerdown', () => this.scene.start('WrongScreen',
           {currentQuestionIndex: ++this.currentQuestionIndex, numCorrect: this.numCorrect, totalQuestions: this.totalQuestions}) );
         }
+        */
+
 }
+  buttonPress(){
+    if ( this.roateDegree == (this.rotateInterval * this.answerIndex) ){
+       this.scene.start('CorrectScreen', 
+      {currentQuestionIndex: ++this.currentQuestionIndex, numCorrect: ++this.numCorrect, totalQuestions: this.totalQuestions}) ;
+    }
+    else {
+      this.scene.start('WrongScreen',
+      {currentQuestionIndex: ++this.currentQuestionIndex, numCorrect: this.numCorrect, totalQuestions: this.totalQuestions}); 
+    }    
+  }
 
   update() {
+    
   }
 }
 
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -9,7 +9,7 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('ninetyDegreeStraightPipe', 'assets/ninetyDegreeStraightPipe.PNG');
     this.load.image('zeroDegreeStraightPipe', 'assets/zeroDegreeStraightPipe.PNG');
     this.load.image('ninetyDegreeButton', 'assets/ninetyDegreeButton.PNG');
-    this.load.image('zeroDegreeButton', 'assets/zeroDegreeButton.PNG');
+    this.load.image('zeroDegreeButton', 'assets/zeroDegreeButton.png');
     this.load.image('buttonLevel1', 'assets/buttonLevel1.PNG');
     this.load.image('buttonLevel2', 'assets/buttonLevel2.PNG');
     this.load.image('buttonLevel3', 'assets/buttonLevel3.PNG');
@@ -18,7 +18,10 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('correctScreen','assets/correctScreen.PNG');
     this.load.image('wrongScreen','assets/wrongScreen.PNG');
     this.load.image('levelComplete', 'assets/levelComplete.PNG');
-
+    this.load.image('pipeWithRing', 'assets/PipeWithRing.PNG');
+    this.load.image('oneEightyDegreeButton', 'assets/oneEightyDegreeButton.PNG');
+    this.load.image('twoSeventyDegreeButton', 'assets/twoSeventyDegreeButton.PNG');
+    this.load.image('submitButton', 'assets/submitButton.png');
     this.load.json('questions', 'assets/data/questions.json');  
   }
 
